f90a51a02b72a43432a5c8998ec9f5ba19dd37f7
grantwbrooks <grantwbrooks@gmail.com>
2017-08-12 15:18:42 -0400

    updates to time displayed

--- apps/showtime/views.py
+++ apps/showtime/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render, HttpResponse, redirect
-from time import gmtime, strftime
+from time import gmtime, strftime, localtime
+from datetime import datetime
 def index(request):
-  context = {
-  "time": strftime("%Y-%m-%d %H:%M %p", gmtime())
-  }
-  return render(request,'showtime/index.html', context)
+#   context = {
+#   "time": strftime("%Y-%m-%d %H:%M %p", localtime())
+#   }
+    context = {
+        "time": datetime.now()
+    }
+#   initially had gmtime() tried out localtime() to see the difference make sure you import at top, can't see the difference need to play with more.
+    # datetime.now seems to work even better, again make sure to import
+    return render(request,'showtime/index.html', context)
